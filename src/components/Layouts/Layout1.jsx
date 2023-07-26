c8f3943d98d56c2a257ded36473697f82c984b81
suronjit797 <suronjit797@gmail.com>
2023-07-27 02:28:05 +0600

    init setup

---
+++ src/components/Layouts/Layout1.jsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
+
+const Layout1 = ({ children, title }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="suronjit797" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout1;
---
+++ src/components/Shared/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div style={{ marginTop: "auto" }}>footer</div>;
+};
+
+export default Footer;
