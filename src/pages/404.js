cb2e049b26a037f44aff4f3217a59dd471b89392
suronjit797 <suronjit797@gmail.com>
2023-07-27 02:52:45 +0600

    not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import styles from '@/styles/NotFound.module.css'
+import React from "react";
+import styles from "@/styles/NotFound.module.css";
+import { Bungee_Spice } from "next/font/google";
+import { useRouter } from "next/router";
+
+const bungee = Bungee_Spice({ subsets: ["latin"], weight: ["400"] });
 
 const NotFoundPage = () => {
-    return (
+  const router = useRouter();
+  return (
+    <section className={styles.container}>
+      <div>
+        <h1 className={`${styles.header} ${bungee.className}`}>404</h1>
+        <h4 className={styles.title}> Not found </h4>
         <div>
-            not found
+          <button onClick={() => router.back()}>Go Back</button>
+          <button onClick={() => router.push("/")}>Go to home</button>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
