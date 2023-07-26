a8b36f11ae4983bbcbee527c72d10e8ef365ff7f
suronjit797 <suronjit797@gmail.com>
2023-07-27 02:40:43 +0600

    auth setup

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/NotFound.module.css'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            not found
+        </div>
+    );
+};
+
+export default NotFoundPage;
